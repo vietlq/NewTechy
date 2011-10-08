48cb9ff5466d4dd4ec24a1f12521775f171f8e86
Le Quoc Viet <vietlq85@gmail.com>
2011-10-08 18:08:51 +0800

    Added more functions & input verification & path validation

--- joom2py/joom2py.py
+++ joom2py/joom2py.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python
 
+import os
+import sys
+import tempfile
+
+"""
+http://stackoverflow.com/questions/847850/cross-platform-way-of-getting-temp-directory-in-python
+
+tempfile.mkdtemp
+"""
+
 class Joom2Py(object):
     def __init__(self, templatePath, destDir = None):
         self.templatePath = templatePath
+        self.destDir = destDir
+        self.tempDir = tempfile.mkdtemp
+        
+        if destDir == None:
+            destDir = os.getcwd()
+    
+    def is_zip(self):
+        return self.templatePath.endswith('.zip')
     
     def set_template_path(self, templatePath):
         self.templatePath = templatePath
@@ -17,13 +35,24 @@ class Joom2Py(object):
         return self.destDir
     
     def process_template(self):
-        pass
+        self.verify_template()
     
     def unzip_template(self):
         pass
     
-    def verify_template(self):
-        pass
+    def verify_template(self):    
+        self.isZip = self.is_zip()
+        
+        if not os.path.exists(self.templatePath):
+            print "The template path does not exist!"
+            return -1
+        
+        if not os.access(self.templatePath, os.R_OK):
+            print "The template path is not readable!"
+            return -2
+        
+        if self.isZip:
+            self.unzip_template()
     
     def move_static(self):
         pass
@@ -63,6 +92,21 @@ class Joom2Py(object):
     
     def zip_template(self):
         pass
+    
+    def write_template(self):
+        pass
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) not in [2, 3]:
+        print "Usage: %s <path_to_template> [destination_folder]" % sys.argv[0]
+    
+    if len(sys.argv) == 2:
+        templatePath = sys.argv[1]
+        destDir = None
+    
+    if len(sys.argv) == 3:
+        templatePath = sys.argv[1]
+        destDir = sys.argv[2]
+    
+    joom2pyObj = Joom2Py(sys.argv[1], destDir)
+    joom2pyObj.process_template()
